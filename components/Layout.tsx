81fcd44a43f1635f786e1480c5beb0d4bdf454f9
Ben Fasoli <benfasoli@gmail.com>
2021-10-29 12:50:36 -0700

    Add iOS PWA meta tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -33,7 +33,12 @@ const Layout = ({ children, title }: Props) => (
         name="description"
         content="Tank asset database for UATAQ lab operations."
       />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="white" />
+
       <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
     </Head>
     <div className="min-h-screen w-full subpixel-antialiased">
